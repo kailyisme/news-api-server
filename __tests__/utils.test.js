3e9e47c083b6a23f154b52464437efe7ba014429
Mike Jackson <mike.jackson@mjconsult.co.uk>
2021-06-15 14:39:50 +0100

    testing half way through

---
+++ __tests__/utils.test.js
@@ -0,0 +1,41 @@
+const { dbLoading } = require("../db/utils/data-manipulation");
+const format = require("pg-format");
+
+describe("dbLoading", () => {
+  test("table name should be the correct one", () => {
+    const table_name = "comments";
+    const data = [
+      {
+        body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: new Date(1586179020000),
+      },
+    ];
+    const result = dbLoading(table_name, data);
+    const expected = `INSERT INTO ${table_name}`;
+    //console.log(result);
+    expect(result.startsWith(expected)).toBeTruthy();
+  });
+  test("check the string literal is correct", () => {
+    const table_name = "comments";
+    const data = [
+      {
+        body: "Oh!",
+        belongs_to: "They're not exactly cats?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: new Date(1586179020000),
+      },
+    ];
+    const result = dbLoading(table_name, data);
+    const keys = Object.keys(data);
+    const values = keys.map((key) => data[key]);
+    //const expected = `${} VALUES (%L)`, keys, values);
+
+    console.log("result = ", result);
+    //console.log(typeof result.indexOf(expected));
+    expect(result.indexOf(expected)).not.toBe(-1);
+  });
+});
