b4fa52cdd616fbc3b7ad4fd3aabb94065619a01c
kaily <kaily.a.newman@gmail.com>
2021-07-12 15:40:25 +0100

    fixed missing import cors on app.js

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const {
   customErrors,
 } = require("./errors_handlers/mainHandlers");
 const mainRouter = require("./routers/mainRouter");
+const cors = require("cors")
 
 const app = express();
+app.use(cors())
+
 app.use(express.json());
 
 app.use(mainRouter);
